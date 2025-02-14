33c420f2e702d5020616135a807dab6f0c8ca66f
Nehemiah <estifanos.tariku@gmail.com>
2025-02-15 02:19:31 +0300

    banner

--- NewNetflix/src/App.jsx
+++ NewNetflix/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-// import Home from './pages/Home/Home'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
+import Home from './pages/Home/Home'
+// import Header from './Components/Header/Header'
+// import Footer from './Components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Footer />  
-
-        
+      <Home />  
     </>
   )
 }
--- NewNetflix/src/pages/Home/Home.jsx
+++ NewNetflix/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Header from '../../Components/Header/Header'
+import Banner from '../../Components/Banner/Banner'
+import Footer from '../../Components/Footer/Footer'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <Header />
+    <Banner />
+    <Footer />
+    </>
   )
 }
 
