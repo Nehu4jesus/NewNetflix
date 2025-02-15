76e47f080f7781d0ecedbfcccccd0e0ec9e644e4
Nehemiah <estifanos.tariku@gmail.com>
2025-02-15 12:28:18 +0300

    commit

--- NewNetflix/src/Components/Footer/Footer.jsx
+++ NewNetflix/src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -13,54 +13,49 @@ const Footer = () => {
           <InstagramIcon />
           <YouTubeIcon />
         </div>
-        <div className='footer_data'>
-          <div className='footer_data ul'>
-            <ul className='footer_data ul li'>
+        <div className='footer_links_container'>
+          <div className='footer_links_column'>
+            <ul>
               <li>Audio Description</li>
-              <li>Investor Relation</li>
+              <li>Investor Relations</li>
               <li>Legal Notices</li>
             </ul>
           </div>
-          <div className='footer_data ul'>
-            <ul className='footer_data ul li'>
+          <div className='footer_links_column'>
+            <ul>
               <li>Help Center</li>
               <li>Jobs</li>
-              <li>Privacy</li>
+              <li>Terms of Use</li>
             </ul>
           </div>
-          <div className='footer_data ul'>
-            <ul className='footer_data ul li'> 
-              <li>Helpp Center</li>
-              <li>Jobs</li>
+          <div className='footer_links_column'>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Corporate Information</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className='footer_links_column'>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
               <li>Cookie Preferences</li>
             </ul>
           </div>
-          <div className='footer_data ul'>
-            <ul className='footer_data ul li'>
-              <li>Gift Cards</li>
-              <li>Terms of Use</li>
-              <li>Cooprate Information</li>
-            </ul>
         </div>
-        <div className='footer_data ul'>
-          <ul className='footer_data ul li'>
-            <li>Media Center</li>
-            <li>Privacy</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div className='service_code'>
-          <p>
-            Service Code
-          </p>
-        </div>
-        <div className='copy_right'>
-          &copy; 1997-2021 Netflix, Inc.
+        <div className='footer_bottom'>
+          <div className='service_code_container'> {/* Container for Service Code and Copyright */}
+            <div className='service_code'>
+              <p>Service Code</p>
+            </div>
+            <div className='copy_right'>
+              &copy; 1997-2024 Netflix, Inc.
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
