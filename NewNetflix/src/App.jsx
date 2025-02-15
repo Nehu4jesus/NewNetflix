00557c5c07cb91d45c54cb86dbe3c70b8ef4cb14
Nehemiah <estifanos.tariku@gmail.com>
2025-02-16 02:22:18 +0300

    commit

--- NewNetflix/src/App.jsx
+++ NewNetflix/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
-// import Header from './Components/Header/Header'
-// import Footer from './Components/Footer/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
