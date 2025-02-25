185ae6ea657e5bf0189846c7134b909926585096
Nehu4jesus <enehemiya@yahoo.com>
2025-02-26 02:44:05 +0300

    RowList

--- NewNetflix/src/Components/Rows/RowList/RowList.jsx
+++ NewNetflix/src/Components/Rows/RowList/RowList.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+// RowList.jsx
+import React from "react";
 import "./RowList.css";
-import Row from '../Row/Row';
-import request from '../../../utils/Request';
+import Row from "../Row/Row";
+import request from "../../../utils/Request";
 
 const RowList = () => {
   return (
     <>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow={true} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={request.fetchTopRatedMovies} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />{" "}
+      {/* Fixed from fetchTopRatedMovies */}
       <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
       <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} /> 
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
       <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="TV Shows" fetchUrl={request.fetchTvShow} />
+      <Row title="TV Shows" fetchUrl={request.fetchTvShows} />{" "}
+      {/* Fixed from fetchTvShow */}
       <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </>
   );
